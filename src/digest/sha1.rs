0262ef85d8361dc7d29315b7db120479de406087
Ajay Eeralla <ajay.eeralla@gmail.com>
2022-06-19 22:01:45 -0700

    add proof specs for sha1

--- src/digest/sha1.rs
+++ src/digest/sha1.rs
@@ -67,10 +67,10 @@ fn block_data_order_(mut H: State, M: &[[<W32 as Word>::InputBytes; 16]]) -> Sta
         let e = H[4];
 
         // FIPS 180-4 6.1.2 Step 3 with constants and functions from FIPS 180-4 {4.1.1, 4.2.1}
-        let (a, b, c, d, e) = step3(a, b, c, d, e, W[ 0..20].try_into().unwrap(), Wrapping(0x5a827999), ch);
-        let (a, b, c, d, e) = step3(a, b, c, d, e, W[20..40].try_into().unwrap(), Wrapping(0x6ed9eba1), parity);
-        let (a, b, c, d, e) = step3(a, b, c, d, e, W[40..60].try_into().unwrap(), Wrapping(0x8f1bbcdc), maj);
-        let (a, b, c, d, e) = step3(a, b, c, d, e, W[60..80].try_into().unwrap(), Wrapping(0xca62c1d6), parity);
+        let (a, b, c, d, e) = step3_ch(a, b, c, d, e, W[ 0..20].try_into().unwrap());
+        let (a, b, c, d, e) = step3_parity1(a, b, c, d, e, W[20..40].try_into().unwrap());
+        let (a, b, c, d, e) = step3_maj(a, b, c, d, e, W[40..60].try_into().unwrap());
+        let (a, b, c, d, e) = step3_parity2(a, b, c, d, e, W[60..80].try_into().unwrap());
 
         // FIPS 180-4 6.1.2 Step 4
         H[0] += a;
@@ -81,6 +81,90 @@ fn block_data_order_(mut H: State, M: &[[<W32 as Word>::InputBytes; 16]]) -> Sta
     }
 
     H
+}
+
+#[inline(always)]
+fn step3_ch(
+    mut a: W32,
+    mut b: W32,
+    mut c: W32,
+    mut d: W32,
+    mut e: W32,
+    W: [W32; 20],
+) -> (W32, W32, W32, W32, W32) {
+    let k = Wrapping(0x5a827999);
+    for W_t in W.iter() {
+        let T = rotl(a, 5) + ch(b, c, d) + e + k + W_t;
+        e = d;
+        d = c;
+        c = rotl(b, 30);
+        b = a;
+        a = T;
+    }
+    (a, b, c, d, e)
+}
+
+#[inline(always)]
+fn step3_parity1(
+    mut a: W32,
+    mut b: W32,
+    mut c: W32,
+    mut d: W32,
+    mut e: W32,
+    W: [W32; 20],
+) -> (W32, W32, W32, W32, W32) {
+    let k = Wrapping(0x6ed9eba1);
+    for W_t in W.iter() {
+        let T = rotl(a, 5) + parity(b, c, d) + e + k + W_t;
+        e = d;
+        d = c;
+        c = rotl(b, 30);
+        b = a;
+        a = T;
+    }
+    (a, b, c, d, e)
+}
+
+#[inline(always)]
+fn step3_parity2(
+    mut a: W32,
+    mut b: W32,
+    mut c: W32,
+    mut d: W32,
+    mut e: W32,
+    W: [W32; 20],
+) -> (W32, W32, W32, W32, W32) {
+    let k = Wrapping(0xca62c1d6);
+    for W_t in W.iter() {
+        let T = rotl(a, 5) + parity(b, c, d) + e + k + W_t;
+        e = d;
+        d = c;
+        c = rotl(b, 30);
+        b = a;
+        a = T;
+    }
+    (a, b, c, d, e)
+}
+
+#[inline(always)]
+fn step3_maj(
+    mut a: W32,
+    mut b: W32,
+    mut c: W32,
+    mut d: W32,
+    mut e: W32,
+    W: [W32; 20],
+) -> (W32, W32, W32, W32, W32) {
+    let k = Wrapping(0x8f1bbcdc);
+    for W_t in W.iter() {
+        let T = rotl(a, 5) + maj(b, c, d) + e + k + W_t;
+        e = d;
+        d = c;
+        c = rotl(b, 30);
+        b = a;
+        a = T;
+    }
+    (a, b, c, d, e)
 }
 
 #[inline(always)]
@@ -105,13 +189,71 @@ fn step3(
     (a, b, c, d, e)
 }
 
+fn cryptol_step3(
+    mut a: W32,
+    mut b: W32,
+    mut c: W32,
+    mut d: W32,
+    mut e: W32,
+    W: [W32; 20],
+    k: W32,
+    f: impl Fn(W32, W32, W32) -> W32,
+) -> (W32, W32, W32, W32, W32) {
+    for W_t in W.iter() {
+        let T = rotl(a, 5) + f(b, c, d) + e + k + W_t;
+        e = d;
+        d = c;
+        c = rotl(b, 30);
+        b = a;
+        a = T;
+    }
+    (a, b, c, d, e)
+}
+
+
 #[inline(always)]
 fn rotl(x: W32, n: u32) -> W32 {
     Wrapping(x.0.rotate_left(n))
 }
 
-fn myrotl(x: u32, n: u32) -> u32 {
-    x.rotate_left(n)
+#[cfg(crux)]
+pub mod cry{
+    extern crate crucible;
+    use crucible::*;
+    // use super::crucible::cryptol;
+    pub type btype = ([u32; 5], [u32; 16]);
+    pub type step3_type = (u32, u32, u32, u32, u32, [u32; 20]);
+    cryptol! {
+        path "Primitive::Keyless::Hash::SHA1";
+
+        pub fn process_block(hm: btype) -> [u32; 5]
+            = "block";
+
+        pub fn rotl(a: u32, n: u32) -> u32
+            = r#" \(a: [32]) (n: [32]) -> (a <<< n)"#;
+
+        pub fn parity(a: u32, b: u32, c: u32) -> u32
+            = r#"\(a: [32]) (b: [32]) (c: [32]) -> (a ^ b ^ c)"#;
+
+        pub fn ch(x: u32, y: u32, z: u32) -> u32
+            = r#"\(x: [32]) (y: [32]) (z: [32]) -> (x && y) || (~x && z)"#;
+
+        pub fn maj(x: u32, y: u32, z: u32) -> u32
+            = r#"\(x: [32]) (y: [32]) (z: [32]) -> (x && y) || (x && z) || (y && z)"#;
+
+        pub fn step3_ch (arg : step3_type) -> (u32, u32, u32, u32, u32)
+            = "step3_ch";
+
+        pub fn step3_maj (arg : step3_type) -> (u32, u32, u32, u32, u32)
+            = "step3_maj";
+
+        pub fn step3_parity1 (arg : step3_type) -> (u32, u32, u32, u32, u32)
+            = "step3_parity1";
+
+        pub fn step3_parity2 (arg : step3_type) -> (u32, u32, u32, u32, u32)
+            = "step3_parity2";
+    }
+
 }
 
 #[cfg(crux)]
@@ -124,21 +266,17 @@ mod crux_test {
     use crucible_spec_macro::crux_spec_for;
     use super::*;
 
-    mod cry {
-        use super::crucible::cryptol;
-        cryptol! {
-            path "Primitive::Keyless::Hash::SHA1";
-
-            pub fn process_block(h: [u32; 5], m: [u32; 16]) -> [u32; 8]
-                = "block";
-
-            pub fn rotl(a: u32, n: u32) -> u32
-                = r#" \a n -> (a <<< n)"#;
-
-            pub fn parity(a: u32, b: u32, c: u32) -> u32
-                = r#"\a b c -> (a ^ b ^ c)"#;
-        }
-    }
+//     use rand::Rng;
+//     #[test]
+//     fn step3_ch_eq(){
+//         let mut rng = rand.thread_rng();
+//         let [a, b, c, d, e] = rng.gen::<[u32; 5]>();
+//         let w = rng.gen::<[u32; 20]>();
+//         let output_real = step3_ch(a, b, c, d, e, w);
+//         let output_cryptol = cryptol_step3_ch(Wrapping(a), Wrapping(b), Wrapping(c), Wrapping(d),
+//         Wrapping(e), w.iter().map(|x| Wrapping(x)).collect());
+//         assert_eq!(output_real, output_cryptol);
+// }
 
     fn wrap_slice<T: Copy>(src: &[T], dest: &mut [Wrapping<T>]) {
         assert!(src.len() == dest.len());
@@ -147,12 +285,12 @@ mod crux_test {
         }
     }
 
-    // fn cryptol_rotl(
-    //     a: W32,
-    //     n: W32
-    // ) -> W32 {
-    //     Wrapping(cry::rotl(a, n))
-    // }
+    fn cryptol_rotl(
+        a: W32,
+        n: u32
+    ) -> W32 {
+        Wrapping(cry::rotl(a.0, n))
+    }
 
     fn cryptol_parity(
         a: W32,
@@ -162,6 +300,108 @@ mod crux_test {
         Wrapping(cry::parity(a.0, b.0, c.0))
     }
 
+    fn cryptol_ch(
+        a: W32,
+        b: W32,
+        c: W32
+    ) -> W32 {
+        Wrapping(cry::ch(a.0, b.0, c.0))
+    }
+
+    fn cryptol_maj(
+        a: W32,
+        b: W32,
+        c: W32
+    ) -> W32 {
+        Wrapping(cry::maj(a.0, b.0, c.0))
+    }
+
+    #[inline(always)]
+    fn cryptol_step3_ch(
+    mut a: W32,
+    mut b: W32,
+    mut c: W32,
+    mut d: W32,
+    mut e: W32,
+    W: [W32; 20],
+    ) -> (W32, W32, W32, W32, W32) {
+    let mut ws = [0u32; 20];
+    for i in 0..20 {
+        ws[i] = W[i].0;
+    }
+    let res = cry::step3_ch((a.0, b.0, c.0, d.0, e.0, ws));
+    (Wrapping(res.0), Wrapping(res.1), Wrapping(res.2), Wrapping(res.3), Wrapping(res.4))
+    }
+
+    #[inline(always)]
+    fn cryptol_step3_maj(
+    mut a: W32,
+    mut b: W32,
+    mut c: W32,
+    mut d: W32,
+    mut e: W32,
+    W: [W32; 20],
+    ) -> (W32, W32, W32, W32, W32) {
+    let mut ws = [0u32; 20];
+    for i in 0..20 {
+        ws[i] = W[i].0;
+    }
+    let res = cry::step3_maj((a.0, b.0, c.0, d.0, e.0, ws));
+    (Wrapping(res.0), Wrapping(res.1), Wrapping(res.2), Wrapping(res.3), Wrapping(res.4))
+    }
+    #[inline(always)]
+    fn cryptol_step3_parity1(
+    mut a: W32,
+    mut b: W32,
+    mut c: W32,
+    mut d: W32,
+    mut e: W32,
+    W: [W32; 20],
+    ) -> (W32, W32, W32, W32, W32) {
+    let mut ws = [0u32; 20];
+    for i in 0..20 {
+        ws[i] = W[i].0;
+    }
+    let res = cry::step3_parity1((a.0, b.0, c.0, d.0, e.0, ws));
+    (Wrapping(res.0), Wrapping(res.1), Wrapping(res.2), Wrapping(res.3), Wrapping(res.4))
+    }
+
+    #[inline(always)]
+    fn cryptol_step3_parity2(
+    mut a: W32,
+    mut b: W32,
+    mut c: W32,
+    mut d: W32,
+    mut e: W32,
+    W: [W32; 20],
+    ) -> (W32, W32, W32, W32, W32) {
+    let mut ws = [0u32; 20];
+    for i in 0..20 {
+        ws[i] = W[i].0;
+    }
+    let res = cry::step3_parity2((a.0, b.0, c.0, d.0, e.0, ws));
+    (Wrapping(res.0), Wrapping(res.1), Wrapping(res.2), Wrapping(res.3), Wrapping(res.4))
+    }
+
+   // proved
+    #[crux_spec_for(ch)]
+    fn ch_equiv(){
+        let [a, b, c] = <[W32; 3]>::symbolic("input");
+        let output_real = munge(ch(a, b, c));
+        let output_cryptol = munge(cryptol_ch(a, b, c));
+        crucible_assert!(output_real == output_cryptol);
+    }
+
+    // proved
+    #[crux_spec_for(maj)]
+    fn maj_equiv(){
+        let [a, b, c] = <[W32; 3]>::symbolic("input");
+        let output_real = munge(maj(a, b, c));
+        let output_cryptol = munge(cryptol_maj(a, b, c));
+        crucible_assert!(output_real == output_cryptol);
+    }
+
+    // proved
     #[crux_spec_for(parity)]
     fn parity_equiv(){
         let [a, b, c] = <[W32; 3]>::symbolic("input");
@@ -170,21 +410,82 @@ mod crux_test {
         crucible_assert!(output_real == output_cryptol);
     }
 
-    #[crux_test]
+    // proved
+    #[crux_spec_for(rotl)]
     fn rotl_equiv(){
-        let a =<u32>::symbolic("a");
+        let a =<W32>::symbolic("a");
         let n = <u32>::symbolic("n");
-        let output_real = munge(myrotl(a, n));
-        let output_cryptol = munge(cry::rotl(a, n));
+        let output_real = munge(rotl(a, n));
+        let output_cryptol = munge(cryptol_rotl(a, n));
         crucible_assert!(output_real == output_cryptol);
     }
 
+    // counter example
+    // #[crux_spec_for(step3_ch)]
+    #[crux_test]
+    fn step3_ch_equiv(){
+        rotl_equiv_spec().enable();
+        ch_equiv_spec().enable();
+        let [a, b, c, d, e] = <[W32; 5]>::symbolic("input");
+        let w = <[W32; 20]>::symbolic("ws");
+        let output_real = step3_ch(a, b, c, d, e, w);
+        let output_cryptol = cryptol_step3_ch(a, b, c, d, e, w);
+        crucible_assert!(output_real == output_cryptol);
+    }
+
+    // counter example
+    //#[crux_spec_for(step3_maj)]
+    #[crux_test]
+    fn step3_maj_equiv(){
+        rotl_equiv_spec().enable();
+        maj_equiv_spec().enable();
+        let [a, b, c, d, e] = <[W32; 5]>::symbolic("input");
+        let w = <[W32; 20]>::symbolic("ws");
+        let output_real = step3_maj(a, b, c, d, e, w);
+        let output_cryptol = cryptol_step3_maj(a, b, c, d, e, w);
+        crucible_assert!(output_real == output_cryptol);
+    }
+
+    // counter example
+    // #[crux_spec_for(step3_parity1)]
+    #[crux_test]
+    fn step3_parity1_equiv(){
+        rotl_equiv_spec().enable();
+        parity_equiv_spec().enable();
+        let [a, b, c, d, e] = <[W32; 5]>::symbolic("input");
+        let mut w = <[u32; 20]>::symbolic("ws");
+        let mut w_wrap = [Wrapping(0); 20];
+        let output_real = step3_parity1(a, b, c, d, e, w_wrap);
+        let output_cryptol = cryptol_step3_parity1(a, b, c, d, e, w_wrap);
+        crucible_assert!(output_real == output_cryptol);
+    }
+
+    // counter example
+    //#[crux_spec_for(step3_parity2)]
+    #[crux_test]
+    fn step3_parity2_equiv(){
+        rotl_equiv_spec().enable();
+        parity_equiv_spec().enable();
+        let [a, b, c, d, e] = <[W32; 5]>::symbolic("input");
+        let w = <[W32; 20]>::symbolic("ws");
+        let output_real = step3_parity2(a, b, c, d, e, w);
+        let output_cryptol = cryptol_step3_parity2(a, b, c, d, e, w);
+        crucible_assert!(output_real == output_cryptol);
+    }
+
+    // taking time to terminate
     #[crux_test]
     fn block_data_order_equiv() {
         parity_equiv_spec().enable();
-        //rotl_equiv_spec().enable();
+        rotl_equiv_spec().enable();
+        ch_equiv_spec().enable();
+        maj_equiv_spec().enable();
+        override_(cryptol_step3_ch, step3_ch);
+        override_(cryptol_step3_maj, step3_maj);
+        override_(cryptol_step3_parity1, step3_parity1);
+        override_(cryptol_step3_parity2, step3_parity2);
 
-        let state = <[u32; 5]>::symbolic("block");
+        let state = <[u32; 5]>::symbolic("state");
         let mut state_wrap = [Wrapping(0); 5];
         wrap_slice(&state, &mut state_wrap);
 
@@ -194,16 +495,10 @@ mod crux_test {
 
         let mut block_split_byte_array = [[0u8; 4]; 16];
         let output_real = munge(block_data_order_(state_wrap, &[block_split_byte_array]));
-        let output_cryptol = munge(cry::process_block(state, block));
+        let output_cryptol = munge(cry::process_block((state, block)));
 
         for (x, y) in output_real.iter().zip(output_cryptol.iter()) {
             crucible_assert!(x.0 == *y);
         }
     }
-
-
-
-
-
-
 }
